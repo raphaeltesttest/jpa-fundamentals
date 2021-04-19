dcadfba86ee9f10ab08806abf478f9e8492125f0
marianos <marianos8125@gmail.com>
2021-04-19 08:13:36 -0300

    add course

---
+++ jpa-c3/src/main/java/entities/Price.java
@@ -0,0 +1,41 @@
+package entities;
+
+import javax.persistence.*;
+import java.util.Currency;
+
+@Entity
+public class Price {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private double amount;
+
+    @Enumerated(EnumType.STRING)
+    private Currency currency;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+}
