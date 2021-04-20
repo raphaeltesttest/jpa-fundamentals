8dbe9682a38d0fa1dd592579a09e5cc5f3ea9c6b
marianos <marianos8125@gmail.com>
2021-04-20 13:06:43 -0300

    add course

---
+++ jpa-c10/src/main/java/entities/Employee.java
@@ -0,0 +1,33 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Employee {
+
+
+    @Id
+    private int id;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ jpa-c10/src/main/java/main/Main.java
@@ -0,0 +1,67 @@
+package main;
+
+import entities.Animal;
+import entities.Bicycle;
+import entities.Car;
+import entities.Cat;
+
+import javax.persistence.Persistence;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+        var em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+//        Animal a = new Animal();
+//        a.setName("A");
+//
+//        Cat c = new Cat();
+//        c.setName("B");
+//        c.setColor("Black");
+//
+//        em.persist(a);
+//        em.persist(c);
+
+//        Product p = new Product();
+//        p.setName("P");
+//
+//        Chocolate c = new Chocolate();
+//        c.setName("C");
+//        c.setkCal(100);
+//
+//        em.persist(p);
+//        em.persist(c);
+
+//        Employee e = new Employee();
+//        e.setId(1);
+//        e.setName("Bill");
+//
+//        Manager m = new Manager();
+//        m.setId(2);
+//        m.setName("Mary");
+//        m.setResponsibility("smth");
+//
+//        em.persist(e);
+//        em.persist(m);
+
+        Car c = new Car();
+        c.setColor("Black");
+        c.setGas("Diesel");
+
+        Bicycle b = new Bicycle();
+        b.setColor("Red");
+        b.setModel("BMX");
+
+        em.persist(c);
+        em.persist(b);
+
+        em.getTransaction().commit();
+        em.close();
+
+
+    }
+}
