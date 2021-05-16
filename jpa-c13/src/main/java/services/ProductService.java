ae994bed489c23710728a925d6a5edeaa2a5f5e2
marianos <marianos8125@gmail.com>
2021-05-15 22:47:19 -0300

    add course

--- jpa-c13/src/main/java/services/ProductService.java
+++ jpa-c13/src/main/java/services/ProductService.java
@@ -1,7 +1,11 @@
 package services;
 
+import entities.Product;
+import repositories.ProductRepository;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class ProductService {
 
@@ -9,5 +13,29 @@ public class ProductService {
 
     public ProductService() {
         this.emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+    }
+    public void addProduct(String name) {
+        var em = emf.createEntityManager();
+        var repository = new ProductRepository(em);
+
+        Product p = new Product();
+        p.setName(name);
+
+        try {
+            em.getTransaction().begin();
+            repository.addProduct(p);
+            em.getTransaction().commit();
+        } catch (RuntimeException e) {
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Product> findProducts() {
+        var em = emf.createEntityManager();
+        var repository = new ProductRepository(em);
+
+        return repository.findAllProducts();
     }
 }
