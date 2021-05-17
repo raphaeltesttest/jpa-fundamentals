9cffae7d0f2bf828aad1f8f49150fb036cd18fa9
marianos <marianos8125@gmail.com>
2021-05-17 14:01:21 -0300

    add course

--- jpa-c13/src/main/java/main/Main.java
+++ jpa-c13/src/main/java/main/Main.java
@@ -5,5 +5,10 @@ import services.ProductService;
 public class Main {
     public static void main(String[] args) {
         ProductService service = new ProductService();
+
+        service.addProduct("Beer");
+        service.addProduct("Chocolate");
+
+        service.findProducts().forEach(System.out::println);
     }
 }
