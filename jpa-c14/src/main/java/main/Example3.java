f7f5694095ee0da168ccd3866c37cbffc1e00bec
marianos <marianos8125@gmail.com>
2021-05-30 22:23:05 -0300

    add course

---
+++ jpa-c14/src/main/java/main/Example3.java
@@ -0,0 +1,21 @@
+package main;
+
+import entities.Product;
+
+import javax.persistence.Persistence;
+
+public class Example3 {
+    public static void main(String[] args) {
+        var emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+
+        var em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Product p = em.find(Product.class, 1); // post load
+        em.remove(p); // pre remove and post remove
+
+        em.getTransaction().commit(); // DELETE
+        em.close();
+    }
+}
