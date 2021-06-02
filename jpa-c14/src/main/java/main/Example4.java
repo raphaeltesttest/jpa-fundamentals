735eecd9c85bd04bff584693e3be7b10d645d014
marianos <marianos8125@gmail.com>
2021-06-01 22:22:14 -0300

    add course

---
+++ jpa-c14/src/main/java/main/Example4.java
@@ -0,0 +1,24 @@
+package main;
+
+import entities.Product;
+
+import javax.persistence.Persistence;
+
+public class Example4 {
+    public static void main(String[] args) {
+        var emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+
+        var em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+
+        Product p = em.find(Product.class, 2); // goes to L2 cache
+
+        // CACHE
+        var cache = emf.getCache();
+        System.out.println(cache.contains(Product.class, 2)); // false
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
